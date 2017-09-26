8e9eef84401797ae14568a18c2a2f67326a507cf
alexise26 <alexise26@gmail.com>
2017-09-25 22:53:08 -0600

    Delete button added

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Lista, ListaItem} from '../../app/classes/index'
 @Component({
   selector: 'app-agregar',
   templateUrl: 'agregar.component.html',
 })
 export class AgregarComponent implements OnInit {
   nombreLista:string;
-  nombreItem:string;
+  nombreItem:string="";
+
+  items:ListaItem[]=[];
 
   constructor() {  }
 
   ngOnInit() {}
+
+  agregar(){
+    if (this.nombreItem.length==0) {
+        return;
+    }
+    let item =  new ListaItem();
+    item.nombre = this.nombreItem;
+
+    this.items.push(item);
+    this.nombreItem="";
+
+  }
+
+  delete(idx:number){
+
+     this.items.splice(idx,1);
+    //console.log(this.items[idx])
+  }
 }
